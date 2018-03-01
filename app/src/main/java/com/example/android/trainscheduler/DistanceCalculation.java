6f2314cea2561d37350bc4d51dad16c2a9a8df56
dandyunggana123 <ungganadanz@yahoo.com>
2018-03-01 08:38:18 +0700

    Ganti nama  attribut

--- app/src/main/java/com/example/android/trainscheduler/DistanceCalculation.java
+++ app/src/main/java/com/example/android/trainscheduler/DistanceCalculation.java
@@ -3,25 +3,25 @@ package com.example.android.trainscheduler;
 
 public class DistanceCalculation {
     public static final double R= 6371;//in KM
-    private double lat1,lat2,dlat;
-    private double long1,long2,dlong;
+    private double latAwal, latAkhir,dlat;
+    private double longAwal, longAkhir,dlong;
     private double a,c,d;
 
-    public DistanceCalculation(double lat1,double lat2,double long1,double long2){
-        this.lat1 = lat1;
-        this.lat1 = Math.toRadians(this.lat1);
-        this.lat2 = lat2;
-        this.lat2 = Math.toRadians(this.lat2);
-        this.long1 = long1;
-        this.long1 = Math.toRadians(this.long1);
-        this.long2 = long2;
-        this.long2 = Math.toRadians(this.long2);
-        this.dlat = Math.toRadians(lat2-lat1);
-        this.dlong = Math.toRadians(long2-long1);
+    public DistanceCalculation(double latAwal, double latAkhir, double longAwal, double longAkhir){
+        this.latAwal = latAwal;
+        this.latAwal = Math.toRadians(this.latAwal);
+        this.latAkhir = latAkhir;
+        this.latAkhir = Math.toRadians(this.latAkhir);
+        this.longAwal = longAwal;
+        this.longAwal = Math.toRadians(this.longAwal);
+        this.longAkhir = longAkhir;
+        this.longAkhir = Math.toRadians(this.longAkhir);
+        this.dlat = Math.toRadians(latAkhir - latAwal);
+        this.dlong = Math.toRadians(longAkhir - longAwal);
     }
 
     public double getJarak(){
-        this.a = Math.sin(this.dlat/2)*Math.sin(this.dlat/2) + (Math.cos(this.lat1)*Math.cos(this.lat2))*(Math.sin(this.dlong/2)*Math.sin(this.dlong/2));
+        this.a = Math.sin(this.dlat/2)*Math.sin(this.dlat/2) + (Math.cos(this.latAwal)*Math.cos(this.latAkhir))*(Math.sin(this.dlong/2)*Math.sin(this.dlong/2));
         this.c = 2 * (Math.atan2(Math.sqrt(this.a),Math.sqrt(1-this.a)));
         this.d = this.R * this.c;
         return this.d;
